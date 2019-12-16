bc0bf7d4c16699a91f300476b15997abadd9e789
KonstantinEger <konstantineger@outlook.de>
2019-12-16 22:32:28 +0100

    added subjects and func

--- src/grades.site.ts
+++ src/grades.site.ts
@@ -1,23 +1,51 @@
 import { html, TemplateResult } from 'lit-html';
 
 import { Tab, TabNav } from './tabs.comp';
+import { Subject } from './subject.comp';
+
+const subjects: Subject[] = [
+  new Subject('bio', 'Biologie'),
+  new Subject('che', 'Chemie'),
+  new Subject('de', 'Deutsch'),
+  new Subject('en', 'Englisch'),
+  new Subject('eth', 'Ethik'),
+  new Subject('geo', 'Geographie'),
+  new Subject('ge', 'Geschichte'),
+  new Subject('inf', 'Informatik'),
+  new Subject('ma', 'Mathematik'),
+  new Subject('mu', 'Musik'),
+  new Subject('phy', 'Physik'),
+  new Subject('spo', 'Sport')
+];
 
 const tabs: Tab[] = [
   {
     text: 'HJ 1',
-    site: () => html``
+    site: () =>
+      html`
+        ${subjects.map(s => s.render(1))}
+      `
   },
   {
     text: 'HJ 2',
-    site: () => html``
+    site: () =>
+      html`
+        ${subjects.map(s => s.render(2))}
+      `
   },
   {
     text: 'HJ 3',
-    site: () => html``
+    site: () =>
+      html`
+        ${subjects.map(s => s.render(3))}
+      `
   },
   {
     text: 'HJ 4',
-    site: () => html``
+    site: () =>
+      html`
+        ${subjects.map(s => s.render(4))}
+      `
   }
 ];
 const tabnav = new TabNav(...tabs);
---
+++ src/subject.comp.ts
@@ -0,0 +1,102 @@
+import { html } from 'lit-html';
+import { updateApp } from '.';
+
+class Subject {
+  private subjectName: string;
+  private subjectID: string;
+  private exams: number[][];
+  private marks: number[][];
+  private semAvg: number[];
+
+  constructor(subjectID: string, subjectName: string) {
+    this.subjectID = subjectID;
+    this.subjectName = subjectName;
+    this.exams = [[], [], [], []];
+    this.marks = [[], [], [], []];
+    this.semAvg = [-1, -1, -1, -1];
+  }
+
+  public render(semester: number) {
+    return html`
+      <style>
+        .subject {
+          border-bottom: 1px solid #bdbdbd;
+          margin: 5px 0px;
+          padding: 8px;
+        }
+        .subject .avg {
+          float: right;
+        }
+        .subject .subjectbody {
+          padding: 8px;
+        }
+        .subjectbody .marktype {
+          display: inline-block;
+          margin: 3px 0px;
+        }
+        .subject input,
+        .subject button {
+          float: right;
+        }
+        .subject input {
+          width: 40px;
+        }
+      </style>
+      <div class="subject" id=${this.subjectID}>
+        <b>${this.subjectName}</b>
+        <span class="avg">${this.semAvg[semester - 1] < 0 ? '/' : Math.round(this.semAvg[semester - 1])}</span>
+        <br />
+        <div class="subjectbody">
+          <div class="marktype">Klausuren:</div>
+          ${this.exams[semester - 1].map(e => `${e}, `)}
+          <button @click=${this.addExam(semester).bind(this)}>+</button>
+          <input id=${this.subjectID + '-exInput'} type="number" />
+          <br />
+          Noten: ${this.marks[semester - 1].map(m => `${m}, `)}
+          <button @click=${this.addMark(semester).bind(this)}>+</button>
+          <input id=${this.subjectID + '-mkInput'} type="number" />
+        </div>
+      </div>
+    `;
+  }
+
+  private calculateAvg() {
+    for (let s = 0; s < 4; s++) {
+      let toAvg: number[] = [];
+      this.exams[s].map(e => toAvg.push(e, e));
+      this.marks[s].map(m => toAvg.push(m));
+      if (toAvg.length > 0) {
+        const avg = toAvg.reduce((a, v) => a + v, 0) / toAvg.length;
+        this.semAvg[s] = avg;
+      }
+    }
+  }
+
+  private addExam(sem: number) {
+    return () => {
+      const input: HTMLInputElement = document.querySelector('#' + this.subjectID + '-exInput');
+      const newExam: number = parseInt(input.value);
+      if (isNaN(newExam) === false && newExam >= 0 && newExam <= 15) {
+        this.exams[sem - 1].push(newExam);
+        input.value = '';
+        this.calculateAvg();
+        updateApp();
+      }
+    };
+  }
+
+  private addMark(sem: number) {
+    return () => {
+      const input: HTMLInputElement = document.querySelector('#' + this.subjectID + '-mkInput');
+      const newMark: number = parseInt(input.value);
+      if (isNaN(newMark) === false && newMark >= 0 && newMark <= 15) {
+        this.marks[sem - 1].push(newMark);
+        input.value = '';
+        this.calculateAvg();
+        updateApp();
+      }
+    };
+  }
+}
+
+export { Subject };
